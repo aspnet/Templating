6d2279739b2c28ca044dd6467edfb26191a94a6c
Matthijs ter Woord <matthijsterwoord@gmail.com>
2017-10-24 21:22:44 +0200

    First attempt to port the `ASP.NET Core Empty` template from C# to VB.NET.
    This commit targets ASP.NET Core version 2.0.0.

---
+++ src/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/EmptyWeb-VB/Startup.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.AspNetCore.Hosting
+Imports Microsoft.AspNetCore.Http
+Imports Microsoft.Extensions.DependencyInjection
+
+Namespace Company.WebApplication1
+	Public Class Startup
+		' This method gets called by the runtime. Use this method to add services to the container.
+		' For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		Public Sub ConfigureServices(services As IServiceCollection)
+		End Sub
+
+		' This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		Public Sub Configure(app As IApplicationBuilder, env As IHostingEnvironment)
+			If env.IsDevelopment() Then
+				app.UseDeveloperExceptionPage()
+			End If
+
+			app.Run(Async Function(context) 
+				              Await context.Response.WriteAsync("Hello World!")
+						  End Function)
+		End Sub
+	End Class
+End Namespace
