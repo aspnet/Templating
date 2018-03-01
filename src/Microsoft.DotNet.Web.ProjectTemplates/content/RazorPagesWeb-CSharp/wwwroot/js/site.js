4489cfbf183695e1e215923e736051a5123dac4e
Jass Bagga <jbagga93@live.ca>
2018-02-28 16:03:13 -0800

    Formatting

--- src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/wwwroot/js/site.js
+++ src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/wwwroot/js/site.js
@@ -1,2 +1,3 @@
-﻿//Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification for details on configuring this project to bundle and minify static web assets.
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 // Write your Javascript code.
