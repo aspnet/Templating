7cd3da9f7fefed20b8910a5c52c2f8d95bb621f7
Hao Kung <haok@microsoft.com>
2018-03-19 14:33:03 -0700

    Add starter web updates as well

--- migrations/updateMigrations.cmd
+++ migrations/updateMigrations.cmd
@@ -9,3 +9,11 @@ mkdir "Company.WebApplication1"
 dotnet new razor --auth Individual --use-local-db -i ..\artifacts\build\Microsoft.DotNet.Web.ProjectTemplates.*.nupkg -o Company.WebApplication1
 copy "Company.WebApplication1\Data\Migrations\*" "..\src\Microsoft.DotNet.Web.ProjectTemplates\content\RazorPagesWeb-CSharp\Data\SqlServer"
 rd "Company.WebApplication1" /s /q
+mkdir "Company.WebApplication1"
+dotnet new mvc --auth Individual --use-local-db -i ..\artifacts\build\Microsoft.DotNet.Web.ProjectTemplates.*.nupkg -o Company.WebApplication1
+copy "Company.WebApplication1\Data\Migrations\*" "..\src\Microsoft.DotNet.Web.ProjectTemplates\content\StarterWeb-CSharp\Data\SqlServer"
+rd "Company.WebApplication1" /s /q
+mkdir "Company.WebApplication1"
+dotnet new mvc --auth Individual -i ..\artifacts\build\Microsoft.DotNet.Web.ProjectTemplates.*.nupkg -o Company.WebApplication1
+copy "Company.WebApplication1\Data\Migrations\*" "..\src\Microsoft.DotNet.Web.ProjectTemplates\content\StarterWeb-CSharp\Data\SqlLite"
+rd "Company.WebApplication1" /s /q
