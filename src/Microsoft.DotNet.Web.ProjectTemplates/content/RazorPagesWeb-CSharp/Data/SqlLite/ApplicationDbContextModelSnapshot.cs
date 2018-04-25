cbd01ce401c242e051b4b8174eabf5f29e8eaccc
Hao Kung <HaoK@users.noreply.github.com>
2018-04-25 12:41:30 -0700

    Change version to 2.1.0

--- src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Data/SqlLite/ApplicationDbContextModelSnapshot.cs
+++ src/Microsoft.DotNet.Web.ProjectTemplates/content/RazorPagesWeb-CSharp/Data/SqlLite/ApplicationDbContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace Company.WebApplication1.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.0-rc1");
+                .HasAnnotation("ProductVersion", "2.1.0");
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
