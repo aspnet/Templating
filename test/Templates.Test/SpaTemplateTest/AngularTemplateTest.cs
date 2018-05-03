f2d336953e107983914b24cf9d0e3f7800198949
Jass Bagga <jbagga93@live.ca>
2018-05-03 10:31:02 -0700

    [Design] Use RemoteWebDriver

--- test/Templates.Test/SpaTemplateTest/AngularTemplateTest.cs
+++ test/Templates.Test/SpaTemplateTest/AngularTemplateTest.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Testing.xunit;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Testing.xunit;
+using Templates.Test.Helpers;
 using Xunit;
 using Xunit.Abstractions;
 
+[assembly: AssemblyFixture(typeof(SeleniumServerFixture))]
 namespace Templates.Test.SpaTemplateTest
 {
     public class AngularTemplateTest : SpaTemplateTestBase
--- test/Templates.Test/SpaTemplateTest/ReactReduxTemplateTest.cs
+++ test/Templates.Test/SpaTemplateTest/ReactReduxTemplateTest.cs
@@ -1,6 +1,11 @@
-﻿using Xunit;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Templates.Test.Helpers;
+using Xunit;
 using Xunit.Abstractions;
 
+[assembly: AssemblyFixture(typeof(SeleniumServerFixture))]
 namespace Templates.Test.SpaTemplateTest
 {
     public class ReactReduxTemplateTest : SpaTemplateTestBase
