6638dfdb23e4028596c50df47bfcf9cc95445527
Ryan Brandenburg <rybrande@microsoft.com>
2018-06-28 16:23:42 -0700

    Use bootstrap4 for templates

--- src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-CSharp/Controllers/HomeController.cs
+++ src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-CSharp/Controllers/HomeController.cs
@@ -21,13 +21,6 @@ namespace Company.WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
