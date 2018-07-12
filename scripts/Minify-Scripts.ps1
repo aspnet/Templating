fd2b6d210d0442832e135cf2c7136e2194097d03
Ryan Brandenburg <rybrande@microsoft.com>
2018-07-12 10:28:00 -0700

    Update .min.css files

--- scripts/Minify-Scripts.ps1
+++ scripts/Minify-Scripts.ps1
@@ -7,5 +7,27 @@ param()
 Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop'
 
-npm install -g less
+npm install -g uglifycss
+npm install -g uglify-js
 
+$projectContentDir = "$PSScriptRoot/../src/Microsoft.DotNet.Web.ProjectTemplates/content"
+
+$contentDirs = Get-ChildItem -Path $projectContentDir -Directory
+
+foreach ($contentDir in $contentDirs) {
+    $wwwRoot = "$projectContentDir\$contentDir\wwwroot"
+
+    $cssFolder = Join-Path $wwwRoot "css"
+    $siteCss = Join-Path $cssFolder "site.css"
+    $siteMinCss = Join-Path $cssFolder "site.min.css"
+    if (Test-Path $siteCss) {
+        uglifycss $siteCss > $siteMinCss
+    }
+
+    $jsFolder = Join-Path $wwwRoot "js"
+    $siteJs = Join-Path $jsFolder "site.js"
+    $siteMinJs = Join-Path $jsFolder "site.min.js"
+    if (Test-Path $siteJs) {
+        uglifyjs $siteJs --output $siteMinJs
+    }
+}
