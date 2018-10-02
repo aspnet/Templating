7bf75bcdef94468963b1b1105fcfde005a8ef14f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-10-02 16:11:54 +0200

    Updated to react-scripts 2.0.3 and other NPM latest

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/index.js
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/index.js
@@ -6,7 +6,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
@@ -24,6 +24,10 @@ ReactDOM.render(
       <App />
     </ConnectedRouter>
   </Provider>,
-  rootElement);
+  rootElement
+);
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
