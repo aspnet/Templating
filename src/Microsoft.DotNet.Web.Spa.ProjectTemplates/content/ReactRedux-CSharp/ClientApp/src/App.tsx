2b44816d9ee0a4fb979791eea66219537820cfde
Martin Perry <martinpe@microsoft.com>
2018-11-26 07:16:37 -0800

    Update lint command, bump typescript eslint-parser version and make some fixed reported by lint

--- src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
+++ src/Microsoft.DotNet.Web.Spa.ProjectTemplates/content/ReactRedux-CSharp/ClientApp/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Switch, Redirect } from 'react-router';
+import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
@@ -8,7 +8,7 @@ import FetchData from './components/FetchData';
 export default () => (
     <Layout>
         <Route exact path='/' component={Home}/>
-        <Route path='/counter' component={Counter}/>
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData}/>
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
 );
